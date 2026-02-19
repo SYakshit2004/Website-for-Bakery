9c91f24da437acc4074cfc5ee367b94befefe63c
S YAKSHIT <yakshitsimhadrifiles@gmail.com>
2026-02-19 13:00:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function toggleMenu(){
+    document.querySelector(".nav-links").classList.toggle("show");
+}
+
+function addToCart(name, price){
+    let existingItem = cart.find(item => item.name === name);
+
+    if(existingItem){
+        existingItem.quantity += 1;
+    } else {
+        cart.push({
+            name:name,
+            price:price,
+            quantity:1
+        });
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+    updateCartCount();
+    alert(name + " added to cart 🛒");
+}
+
+function updateCartCount(){
+    const count = document.getElementById("cartCount");
+    if(count){
+        count.innerText = cart.length;
+    }
+}
+
+function changeQty(index, change){
+    cart[index].quantity += change;
+
+    if(cart[index].quantity <= 0){
+        cart.splice(index,1);
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+    location.reload();
+}
+
+function removeItem(index){
+    cart.splice(index,1);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    location.reload();
+}
+
+function placeOrder(){
+    let cartData = JSON.parse(localStorage.getItem("cart")) || [];
+
+    if(cartData.length === 0){
+        alert("🛒 Your cart is empty!");
+        return;
+    }
+
+    document.getElementById("orderModal").style.display = "flex";
+
+    localStorage.removeItem("cart");
+}
+
+function closeModal(){
+    document.getElementById("orderModal").style.display = "none";
+    location.reload();
+}
+
+window.onload = function(){
+    updateCartCount();
+}
